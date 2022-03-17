ae55343a209c9a79258932e0670ea4a0cf4f73ea
Andrew Harrison <78227435+aharriso11@users.noreply.github.com>
2022-03-17 17:12:40 +0000

    Initial commit

---
+++ scripts/stats_england.r
@@ -0,0 +1,126 @@
+# stats_england.r
+# Script to build NHSD monthly ATP reporting
+
+# INSTALL PACKAGES ----
+
+# Install the pacman package to call all the other packages
+if (!require("pacman")) install.packages("pacman")
+
+# Use pacman to install (if req) and load required packages
+pacman::p_load(
+  readxl,
+  tidyr,
+  ggplot2,
+  dplyr,
+  zoo,
+  data.table,
+  ggrepel,
+  ggthemes,
+  ggtext
+  )
+
+# SET COMMON VARIABLES ----
+
+# Set ODS code for my Trust
+my_ods <- "RDY"
+
+# set working directory, unique to your machine
+setwd("c:/b/atp_stats")
+
+# Set plot colours
+colour_highexposurescore <- "firebrick2"
+colour_mediumexposurescore <- "sienna2"
+colour_lowexposurescore <- "royalblue3"
+colour_my_trust <- "darkorchid4"
+colour_average <- "black"
+colour_topquartile <- "darkgreen"
+
+# IMPORT DATA ----
+
+# Import monthly data
+atp_stats_long_monthly <- readRDS("atp_stats_long_monthbymonth.rds")
+
+# MUNGE DATA ----
+
+# Set the various data strings we'll need
+current_month <- head(atp_stats_long_monthly$month, 1) # current month for label plotting
+current_month_print <- format(current_month, "%B %Y") # current month for title print
+first_month <- tail(atp_stats_long_monthly$month, 1) # first month for label plotting
+
+# CREATE DATA SUBSETS ----
+
+# My Trust dataset
+my_trust <- atp_stats_long_monthly %>% 
+  filter(ods==my_ods) 
+
+# NHS Trust average dataset
+averages <- atp_stats_long_monthly %>%
+  group_by(month) %>%
+  summarise(cyber_score_mean = mean(cyber_score, na.rm = TRUE))
+
+# NHS Trust top quartile dataset
+topquartile <- atp_stats_long_monthly %>%
+  group_by(month) %>%
+  summarise(top_quartile = quantile(cyber_score, probs = 0.25, na.rm = TRUE))
+
+# Create latest score variables from these subsets to use in the data plot
+latest_my_trust <- head(my_trust$cyber_score, 1)
+latest_average <- tail(averages$cyber_score_mean, 1)
+latest_quartile <- tail(topquartile$top_quartile, 1)
+
+# Left hand side labels dataset
+labels_left_x <- c(first_month, first_month, first_month)
+labels_left_y <- c(29, 69, 100)
+labels_left_text <- c("Low exposure score", "Medium exposure score", "High exposure score")
+labels_left_colour <- c(colour_lowexposurescore, colour_mediumexposurescore, colour_highexposurescore)
+plot_labels_left <- data.table(labels_left_x = labels_left_x, labels_left_y = labels_left_y, labels_left_text = labels_left_text, labels_left_colour = labels_left_colour)
+
+# Right hand side labels dataset
+labels_right_x <- c(current_month, current_month, current_month)
+labels_right_y <- c(latest_my_trust, latest_average, latest_quartile)
+labels_right_text <- c(my_ods, "Average", "Best quartile")
+labels_right_colour <- c(colour_my_trust, colour_average, colour_topquartile)
+plot_labels_right <- data.table(labels_right_x = labels_right_x, labels_right_y = labels_right_y, labels_right_text = labels_right_text, labels_right_colour = labels_right_colour)
+
+# PLOT DATA ----
+
+# Create the plot
+atp_stats_plot <- ggplot() + 
+  # background data
+  geom_line(data = atp_stats_long_monthly, aes(x = month, y = cyber_score, group = ods, colour = ods), colour = alpha("grey75", 0.7)) + 
+  # average line
+  geom_line(data = averages, aes(x = month, y = cyber_score_mean), colour = colour_average, size = 1.5) +
+  # My Trust line
+  geom_line(data = my_trust, aes(x = month, y = cyber_score, group = ods, colour = my_trust), colour = colour_my_trust, size = 2) +
+  # top quartile line
+  geom_line(data = topquartile, aes(x = month, y = top_quartile), colour = colour_topquartile, size = 1.5) +
+  # exposure score lines
+  geom_hline(yintercept = 29, linetype = "dotted", colour = colour_lowexposurescore, size = 2) +
+  geom_hline(yintercept = 69, linetype = "dotted", colour = colour_mediumexposurescore, size = 2) +
+  geom_hline(yintercept = 100, linetype = "dotted", colour = colour_highexposurescore, size = 2) +
+  # left hand side labels
+  geom_label(data = plot_labels_left, aes(x = labels_left_x, y = labels_left_y, label = labels_left_text, group = NULL, hjust = "left"), fill = plot_labels_left$labels_left_colour, colour = "white", fontface = "bold", size = 5, nudge_x = 10) +
+  # right hand side labels
+  geom_text(data = plot_labels_right, aes(x = labels_right_x, y = labels_right_y, label = labels_right_text, group = NULL, hjust = "left"), colour = plot_labels_right$labels_right_colour, fontface = "bold", size = 3, nudge_x = 2.5) +
+  # axis settings
+  scale_x_date(date_labels = "%b %y", date_breaks = "2 months", expand = expansion(mult = c(0, .08))) +
+  scale_y_continuous(breaks = c(20, 40, 60, 80, 100), limits = c(0,100)) +
+  # axis labels
+  xlab("Month") +
+  ylab("Exposure score") +
+  # plot title and subtitle
+  ggtitle(paste("Cyber exposure scores for endpoints - NHS Trusts - January 2020 to", current_month_print, sep = " "), subtitle = "Data supplied by the NHS Digital Data Security Centre") +
+  # plot theme
+  theme_base() +
+  theme(
+    axis.text.x = element_text(angle = 00, size = 9),
+    plot.title = element_text(size = 20, family = "Arial", face = "bold"),
+    plot.subtitle = element_markdown(hjust = 0, vjust = 0, size = 11))
+
+# Draw the plot
+atp_stats_plot 
+
+# EXPORT DATA ----
+
+# Export plot png - A4 size
+ggsave("atp_stats.png", width = 33.867, height = 19.05, units = "cm")
---
+++ scripts/stats_import.r
@@ -0,0 +1,71 @@
+# stats_import.r
+# Written by Andrew Harrison (andrew.harrison11@nhs.net) in a personal capacity
+# for internal use within the NHS only
+
+# CONFIRM THE COMMON VARIABLES BELOW ARE APPROPRIATE FOR YOUR ENVIRONMENT
+# AND THE DATA YOU ARE WORKING WITH
+
+# SET COMMON VARIABLES ----
+
+# set working directory
+setwd("c:/b/atp_stats")
+
+# set the date for the first of the month in the spreadsheet you are importing
+month_import <- "2022-02-01"
+
+# set the path to the spreadsheet
+excel_path <- "raw/2022-02.xlsx"
+
+# INSTALL PACKAGES ----
+
+# Install the pacman package to call all the other packages
+if (!require("pacman")) install.packages("pacman")
+
+# Use pacman to install (if req) and load required packages
+pacman::p_load(
+  readxl,
+  tidyr,
+  ggplot2,
+  dplyr,
+  zoo,
+  data.table
+)
+
+# IMPORT DATA ----
+
+# Import raw data from Excel
+atp_sheet_raw <- read_excel(excel_path, ) 
+
+# MUNGE DATA ----
+
+# Select only the three columns we need
+atp_sheet_raw <- subset(atp_sheet_raw, select = c("ODS Code", "Organisation Type", "Windows Exposure Score"))
+
+# Change column names
+names(atp_sheet_raw)[names(atp_sheet_raw) == "ODS Code"] <- "ods"
+names(atp_sheet_raw)[names(atp_sheet_raw) == "Organisation Type"] <- "orgtype"
+names(atp_sheet_raw)[names(atp_sheet_raw) == "Windows Exposure Score"] <- "cyber_score"
+
+# Filter for types of organisations we want
+atp_sheet_raw <- atp_sheet_raw %>%
+  filter(orgtype=="NHS TRUST")
+
+# Add a new column named 'month' and populate it with the value of month_import
+atp_sheet_raw['month']= month_import
+
+# Set 'month' as a date
+atp_sheet_raw$month <- as.Date(atp_sheet_raw$month, format = "%Y-%m-%d")
+
+# Rearrange columns so they are in the same order as our existing data
+atp_sheet_raw <- subset(atp_sheet_raw, select = c("ods", "month", "cyber_score"))
+
+# MERGE DATA ----
+
+# Import existing data
+atp_stats_long_import <- readRDS("atp_stats_long_monthbymonth.rds")
+
+# Merge the two datasets
+atp_stats_long_monthly <- rbind(atp_sheet_raw, atp_stats_long_import)
+
+# Save the resulting dataset back to the monthly file
+saveRDS(atp_stats_long_monthly, file = "atp_stats_long_monthbymonth.rds")
